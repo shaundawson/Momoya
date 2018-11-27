3615eaba1f7d56f75111e575841858d446400086
Shaun Dawson <shaundawson0883@gmail.com>
2018-11-27 11:52:09 -0500

    React Router installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+
 import Navbar from './Navbar';
 import Hero from './Hero';
 import About from './About';
@@ -9,14 +19,21 @@ import Footer from './Footer';
 
 const App = ()  => {
   return (
+    <Router>
     <div className="App">
     <Hero/>
     <About/>
     <Contact/>
     <Recommended/>
     <Footer/>
+
+    <Route path="/menu" component={Contact} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/gallery" component={Contact} />
+
     </div>
+    </Router>
  );
-};
+}
 
 export default App;
