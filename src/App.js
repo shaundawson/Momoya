65ce36e267359399c8de9081651f713b37115d5a
Shaun Dawson <shaundawson0883@gmail.com>
2018-11-27 17:05:32 -0500

    created menu component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Contact from './Contact';
 import Recommended from './Recommended';
 import Footer from './Footer';
 import Gallery from './Gallery';
+import Menu from './Menu';
 
 const App = () => {
   return (
@@ -24,11 +25,12 @@ const App = () => {
         <Contact />
         <Recommended />
         <Gallery />
+        <Menu />
         <Footer />
         
-        <Route path="/menu" component={Contact} />
+        <Route path="/menu" component={Menu} />
         <Route path="/contact" component={Contact} />
-        <Route path="/gallery" component={Contact} />
+        <Route path="/gallery" component={Gallery} />
 
       </div>
     </Router>
---
+++ src/Menu.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="Menu">
+            <section class="columns">
+                <div class="column">
+                    <h1 class="title is-3">MOMOYA SPECIAL</h1>
+                    <h5 class="title is-5">SAIKORO STEAK</h5>
+                    <p class="subtitle is-6"> frisee, yuzo onion demi-glace 15</p>
+                    <br></br>
+                    <h5 class="title is-5">CHILLED EGGPLANT & SHISHITO</h5>
+                    <p class="subtitle is-6">soy dashi 8</p>
+                    <br></br>
+                    <h5 class="title is-5">WHITE FISH TACO (3pcs)</h5>
+                    <p class="subtitle is-6">yuzu kosho, aji amarillo 12</p>
+                    <br></br>
+                    <h5 class="title is-5">CRISPY LOBSTER TACO (1pcs)</h5>
+                    <p class="subtitle is-6">cilantro, yellow pepper 8</p>
+                    <br></br>
+                    <h5 class="title is-5">PAN ROASTED DUCK</h5>
+                    <p class="subtitle is-6">harico vert, pear balsamic sauce 14</p>
+                    <br></br>
+                    <h5 class="title is-5">CEVICHE SALAD</h5>
+                    <p class="subtitle is-6">white fish, shrimp & octopus 9</p>
+                    <br></br>
+                    <h5 class="title is-5">ROASTED BRUSSEL SPROUTS</h5>
+                    <p class="subtitle is-6">walnut, crispy onion 10</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <h1 class="title is-3">SOUP & SALAD</h1>
+                    <h5 class="title is-5">MISO SOUP</h5>
+                    <p class="subtitle is-6"> Tofu & wakame 3</p>
+                    <br></br>
+                    <h5 class="title is-5">MUSHROOM SOUP</h5>
+                    <p class="subtitle is-6"> Shitake, enoki & button mushroom 6</p>
+                    <br></br>
+                    <h5 class="title is-5">MUSHROOM SOUP</h5>
+                    <p class="subtitle is-6"> Shitake, enoki & button mushroom 6</p>
+                    <br></br>
+                    <h5 class="title is-5">SEAWEED 6</h5>
+                    <br></br>
+                    <h5 class="title is-5">HIKIKI SOY BEAN</h5>
+                    <p class="subtitle is-6"> Tofu sking, carrot, shiitake 7</p>
+                    <br></br>
+                    <h5 class="title is-5">WAKAME & CUCUMBER</h5>
+                    <p class="subtitle is-6"> Ume soy vinaigrette 8</p>
+                    <br></br>
+                    <h5 class="title is-5">SWEET BEER</h5>
+                    <p class="subtitle is-6">Goat cheese, walnut & arugula 10</p>
+                    <br></br>
+                    <h5 class="title is-5">WARM MUSHROOM 13</h5>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                     
+                    <h1 class="title is-3">COLD APPETIZER</h1>
+                    <h5 class="title is-5">GOMAAE</h5>
+                    <p class="subtitle is-6"> spinach w/ sesame 7</p>
+                    <br></br>
+                    <h5 class="title is-5">OHITASHI</h5>
+                    <p class="subtitle is-6"> spinach w/ ponzu 7</p>
+                    <br></br>
+                    <h5 class="title is-5">TAKO SU</h5>
+                    <p class="subtitle is-6"> octopus sashimi in tosazu vinaigrette 12</p>
+                    <br></br>
+                    <h5 class="title is-5">YELLOWTAIL JALAPENO 13</h5>
+                    <br></br>
+                    <h5 class="title is-5">TUNA AVOCADO</h5>
+                    <p class="subtitle is-6"> sesame ginger soy dressing 12</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </div>
+
+                <div class="column">
+                    <h1 class="title is-3">SUSHI BAR ENTREE</h1>
+                    <h5 class="title is-5">SUSHI</h5>
+                    <p class="subtitle is-6"> 8 pieces, choice of 1 roll 29<br></br>
+                        -tuna avocado, salmon avocado, yellowtail
+                         scallion, eel cucumber, tuna</p>
+                    <br></br>
+                    <h5 class="title is-5">SUSHI SASHIMI COMBO</h5>
+                    <p class="subtitle is-6"> choice of 1 roll 32 <br></br>
+                        -tuna avocado, salmon avocado, yellowtail
+                         scallion, eel cucumber, tuna</p>
+                     <br></br>  
+                    <h5 class="title is-5">SASHIMI</h5>
+                    <p class="subtitle is-6">8 kinds of sashimi 30</p>
+                    <br></br>
+                    <h5 class="title is-5">CHIRASHI</h5>
+                    <p class="subtitle is-6">assorted sashimi over sushi rice 30</p>
+                    <br></br>
+                    <h5 class="title is-5">TEKKADON</h5>
+                    <p class="subtitle is-6">tuna sashimi over sushi rice 28</p>
+                    <br></br>
+                    <h5 class="title is-5"><u>CHEF'S SELECTION OF TODAY'S BEST</u></h5>
+                    <br></br>
+                    <h5 class="title is-5">SUSHI TASTING</h5>
+                    <p class="subtitle is-6">10 pcs of sushi, choice of 1 signature roll 60</p>
+                    <br></br>
+                    <h5 class="title is-5">SASHIMI TASTING</h5>
+                    <p class="subtitle is-6">10 kinds of sashimi 65</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+
+                    <h1 class="title is-3">A LA CARTE SPECIALS</h1>
+                    <h5 class="title is-5">KING SALMON 6 KANI (snow crab) 8</h5>
+                    <h5 class="title is-5">KANPACHI (amber jack) 6 MADAI (sea
+                    bream) 6</h5>
+                    <h5 class="title is-5">SHIMA AJI (striped jack) 6 BOTAN EBI
+                    (prawn from canada</h5>
+                    <h5 class="title is-5">KINMEDAI (golden big eye snapper) 7 LIVE
+                    SCALLOP 8</h5>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+                    <h1 class="title is-3">SIGNATURE ROLL</h1>
+                    <h5 class="title is-5">CRISPY RICE</h5>
+                    <p class="subtitle is-6"> spicy tuna, shitake, jalepeno 16</p>
+                    <br></br>
+                    <h5 class="title is-5">GREENWICH</h5>
+                    <p class="subtitle is-6"> spicy yellowtail, asparagus crunch </p>
+                    <br></br>
+                    <h5 class="title is-5">NAKAMURA</h5>
+                    <p class="subtitle is-6"> yellowtail, cucumber topped with tuna & salmon 
+                    w/ spicy scallop 15 </p>
+                    <br></br>
+                    <h5 class="title is-5">LOBSTER TEMPURA</h5>
+                    <p class="subtitle is-6"> avocado, smoked red pepper lobster jus 18 </p>
+                    <br></br>
+                    <h5 class="title is-5">SALMON CRUNCHY</h5>
+                    <p class="subtitle is-6"> amango, avocado & spicy salmon 14 </p>
+                    <br></br>
+                    <h5 class="title is-5">FRESH SPRING</h5>
+                    <p class="subtitle is-6"> shrimp, snow crab, avocado, tomato, red onion & 
+                    cilantro 16 </p>
+                    <br></br>
+                    <h5 class="title is-5">BLACK DYNAMITE</h5>
+                    <p class="subtitle is-6"> spicy tuna, shrimp tempura, cucumber, avocado 
+                    mango sauce 15 </p>
+                    <br></br>
+                    <h5 class="title is-5">MOMOYA SPICY TUNA</h5>
+                    <p class="subtitle is-6"> wrapped in seared yellowtail, almond on top 15</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </div>
+
+                <div class="column">
+                    <h1 class="title is-3">A LA CARTE - SUSHI or SASHIMI</h1>
+                    <strong><p>TUNA 4.5</p>
+                    <br></br>
+                    <p>FLUKE 4</p>
+                    <br></br>
+                    <p>EEL 5</p>
+                    <br></br>
+                    <p>MACKEREL 3</p>
+                    <br></br>
+                    <p>SHRIMP 3</p>
+                    <br></br>
+                    <p>EGG CUSTARD 3</p>
+                    <br></br>
+                    <p>YELLOWTAIL 3</p>
+                    <br></br>
+                    <p>SALMON ROE 5</p>
+                    <br></br>
+                    <p>SEA EEL 6</p></strong>
+                    <br></br>
+                    <br></br>
+
+                    <h1 class="title is-3">ROLLS</h1>
+                    <strong><p>TUNA 6</p>
+                    <br></br>
+                    <p>SPICY TUNA 8</p>
+                    <br></br>
+                    <p>SPICY YELLOWTAIL 8</p>
+                    <br></br>
+                    <p>SALMON AVOCADO 8</p>
+                    <br></br>
+                    <p>SPICY SCALLOP 12</p>
+                    <br></br>
+                    <p>CALIFORNIA 6</p>
+                    <br></br>
+                    <p>RED CRAB CALIFORNIA 6</p>
+                    <br></br>
+                    <h5 class="title is-5">SPIDER</h5>
+                    <p class="subtitle is-7"> crispy soft shell crab, avocado 17</p> 
+                    <br></br>
+                    <h6 class="title is-5">DRAGON</h6>
+                    <p class="subtitle is-7"> eel cucumber wrapped in avocado</p>
+                    <br></br>
+                    <h6 class="title is-5">RAINBOW</h6>
+                    <p class="subtitle is-7"> california roll wrapped in tuna 14</p>
+                    <br></br>
+                    <h6 class="title is-5">BOSTON</h6>
+                    <p class="subtitle is-7"> shrimp, snow crab, asparagus, 
+                    lettuce, cucumber & avocado w/ mayo 14</p>
+                    <br></br>
+                    </strong>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </div>
+
+                <div class="column">
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <strong>
+                    <p>SALMON TROUT 5</p>
+                    <br></br>
+                    <p>SMOKED SALMON 5</p>
+                    <br></br>
+                    <p>FLYING FISH ROE 4 </p>
+                    <br></br>
+                    <p>OCTOPUS 4</p>
+                    <br></br>
+                    <p>SQUID 4</p>
+                    <br></br>
+                    <p>SEA URCHIN 8</p>
+                    <br></br>
+                    <p>TORO 10</p>
+                    </strong>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <strong>
+                    <p>SHRIMP TEMPURA 8</p>
+                    <br></br>
+                    <p>SWEET POTATO TEMPURA 6</p>
+                    <br></br>
+                    <p>ASPARAGUS 5</p>
+                    <br></br>
+                    <p>AVOCADO 5</p>
+                    <br></br>
+                    <p>CUCUMBER 5</p>
+                    <br></br>
+                    <p>NATTO 5</p>
+                    <br></br>
+                    <p>OSHINKO 5</p>
+                    <br></br>
+                    <p>UME SHISO 5</p>
+                    </strong>
+                    <br></br>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Menu
